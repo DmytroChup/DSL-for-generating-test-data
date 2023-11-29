238159677ab9e8cdd9191f07c8f11281389d3299
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2023-11-29 13:31:55 +0100

    initial commit

--- example.rb
+++ example.rb
@@ -6,7 +6,7 @@ user = TestData.entity do
   birth_date :date, range: Date.new(1950, 1, 1)..Date.new(2005, 12, 31)
   gender :choice, array: %w[male female other]
   dad_name :string, length: 10
-  email :email, length: 1
+  email :email, length: 3
   animal :yml, path: "yml/animal.yml"
 end
 
